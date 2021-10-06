8221bf0c58c6a2cb1b15db55dd87428351ac919f
SAEBOMPAK <saebompak@gmail.com>
2021-10-07 02:14:31 +0900

    add firebase.js

---
+++ firebase.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD5JEqRESB2z92xhbK-Oue2a4giaSv8GAU",
+    authDomain: "to-do-live-dec31.firebaseapp.com",
+    projectId: "to-do-live-dec31",
+    storageBucket: "to-do-live-dec31.appspot.com",
+    messagingSenderId: "473774381336",
+    appId: "1:473774381336:web:677db1d44686b62688666d",
+    measurementId: "G-YP7TN9S7FY"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
