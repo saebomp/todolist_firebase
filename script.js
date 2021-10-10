d808a4681fed6f5bf64bfd0d90d198702ac940ae
SAEBOMPAK <saebompak@gmail.com>
2021-10-11 03:38:31 +0900

    add getItems

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
-const form = document.getElementById('submit-form')
-form.addEventListener('submit', addItem);
 
-function addItem(e){
+addItem = (e) => {
     e.preventDefault();
     let text = document.getElementById("todo-input")
     db.collection("todo-items").add({
         text: text.value,
         status:"active"
     })
-    
+    text.value = "";
 }
+
+const form = document.getElementById('submit-form')
+form.addEventListener('submit', addItem);
+
+//realtime
+getItems = () => {
+    db.collection("todo-items").onSnapshot((snapshot) => {
+        // console.log(snapshot)
+        let items = [];
+        snapshot.docs.forEach((doc)=> {
+            items.push({
+                id:doc.id,
+                ...doc.data()
+            })
+        })
+        console.log(items)
+    })
+}
+
+getItems();
